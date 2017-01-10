6645eb6fc4be4ef6be3edbf7430e27b8ec17a352
jorge <jorgearteaga8936@gmail.com>
2017-01-10 19:28:01 -0400

    able to run script from another directory

--- restore_keyboard.sh
+++ restore_keyboard.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo rm /usr/share/X11/xkb/symbols/us;
-sudo cp ./original_us_layout /usr/share/X11/xkb/symbols/us;
-sudo rm /usr/share/X11/xkb/symbols/pc;
-sudo cp ./original_pc105_model /usr/share/X11/xkb/symbols/pc;
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+sudo rm /usr/share/X11/xkb/symbols/us;
+sudo cp "$DIR/original_us_layout" /usr/share/X11/xkb/symbols/us;
+sudo rm /usr/share/X11/xkb/symbols/pc;
+sudo cp "$DIR/original_pc105_model" /usr/share/X11/xkb/symbols/pc;
--- restrict_keyboard.sh
+++ restrict_keyboard.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo rm /usr/share/X11/xkb/symbols/us;
-sudo cp ./modified_us_layout /usr/share/X11/xkb/symbols/us;
-sudo rm /usr/share/X11/xkb/symbols/pc;
-sudo cp ./modified_pc105_model /usr/share/X11/xkb/symbols/pc;
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+sudo rm /usr/share/X11/xkb/symbols/us;
+sudo cp "$DIR/modified_us_layout" /usr/share/X11/xkb/symbols/us;
+sudo rm /usr/share/X11/xkb/symbols/pc;
+sudo cp "$DIR/modified_pc105_model" /usr/share/X11/xkb/symbols/pc;
