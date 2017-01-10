6aaf82f35f33f925b263bf227d9746c830d292a6
jorge <jorgearteaga8936@gmail.com>
2017-01-10 13:15:27 -0400

    disable sensitive keys from keyboard model

--- OSRS.sh
+++ OSRS.sh
@@ -1,5 +1,5 @@
 sudo rm /usr/share/X11/xkb/symbols/us;
 sudo cp ./us /usr/share/X11/xkb/symbols/us;
 sudo rm /usr/share/X11/xkb/symbols/pc;
-sudo cp ./us /usr/share/X11/xkb/symbols/pc;
+sudo cp ./pc /usr/share/X11/xkb/symbols/pc;
 
